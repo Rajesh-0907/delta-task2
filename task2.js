5578b45ca753b0804153ec658eb79b72072ecfa8
Rajesh-0907 <rajeshraichu529@gmail.com>
2020-08-20 11:52:57 +0530

    delta

---
+++ task2.js
@@ -0,0 +1,179 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+var i = 0;
+var text = "";
+var res = "";
+var j = 0;
+var res1 = "";
+var equ = document.getElementById('equa');
+var k = parseInt("x+5");
+console.log(k);
+
+
+
+function myFunction(){
+  ctx.clearRect(0, 0, 600, 600);
+  ctx.beginPath();
+  ctx.moveTo(300, 0);
+  ctx.lineTo(300, 600);
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(0, 300);
+  ctx.lineTo(600, 300);
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+ctx.fillText("x", 590, 310);
+ctx.fillText("y", 290, 10);
+ctx.fillText("-x", 8, 310);
+ctx.fillText("-y", 285, 590);
+ctx.fillText("O", 305, 315);
+
+
+  ctx.translate(300, 300)
+  text = equ.value;
+  res = text.toUpperCase();
+  res1 = text.split(" ");
+  for(j = 0; j < text.length; j++)
+{
+ console.log(res1[j]);
+}
+ console.log(res);
+
+if(res === ""){
+  window.alert("equation required!!")
+  ctx.translate(-300, -300)
+  ctx.clearRect(0, 0, 600, 600);
+}
+else if(res === "X"){
+  ctx.save();
+  ctx.save();
+  ctx.beginPath();
+
+
+  ctx.scale(1, 1);
+  ctx.moveTo(-300, 300 );
+  for(i = -300; i < 300; i++)
+  {
+  ctx.lineTo(i , -i);
+  }
+
+  ctx.restore();
+  ctx.lineJoin = "round";
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.restore();
+  ctx.translate(-300, -300)
+}
+else if (res === "-X") {
+  ctx.save();
+  ctx.save();
+  ctx.beginPath();
+
+
+  ctx.scale(1, 1);
+  ctx.moveTo(-300, -300 );
+  for(i = -300; i < 300; i++)
+  {
+  ctx.lineTo(i , i);
+  }
+
+  ctx.restore();
+  ctx.lineJoin = "round";
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.restore();
+  ctx.translate(-300, -300)
+}
+ else if(res === "X*X")
+ {ctx.save();
+ ctx.save();
+ ctx.beginPath();
+
+
+ ctx.scale(10, 40);
+ ctx.moveTo(-76, -19 * 19);
+ for(i = -76; i < 76; i++)
+ {
+ ctx.lineTo(i , -i/4 * i/4);
+ }
+
+ ctx.restore();
+ ctx.lineJoin = "round";
+ ctx.strokeStyle = "red";
+ ctx.lineWidth = 2;
+ ctx.stroke();
+ ctx.restore();
+ ctx.translate(-300, -300)
+ }
+
+ else if (res === "SINX") {
+   ctx.save();
+   ctx.save();
+   ctx.beginPath();
+
+
+   ctx.scale(5, 50);
+   ctx.moveTo(-76, -Math.sin(19));
+   for(i = -76; i < 76; i++)
+   {
+   ctx.lineTo(i , -Math.sin(i/4));
+   }
+
+   ctx.restore();
+   ctx.lineJoin = "round";
+   ctx.strokeStyle = "red";
+   ctx.lineWidth = 2;
+   ctx.stroke();
+   ctx.restore();
+   ctx.translate(-300, -300)
+ }
+ else if (res === "COSX") {
+   ctx.save();
+   ctx.save();
+   ctx.beginPath();
+
+
+   ctx.scale(5, 50);
+   ctx.moveTo(-76, -Math.cos(19));
+   for(i = -76; i < 76; i++)
+   {
+   ctx.lineTo(i , -Math.cos(i/4));
+   }
+
+   ctx.restore();
+   ctx.lineJoin = "round";
+   ctx.strokeStyle = "red";
+   ctx.lineWidth = 2;
+   ctx.stroke();
+   ctx.restore();
+   ctx.translate(-300, -300)
+ }
+ else if (res === "X*X*X") {
+   ctx.save();
+   ctx.save();
+   ctx.beginPath();
+
+
+   ctx.scale(10, 40);
+   ctx.moveTo(-76, -19 * 19 * 19);
+   for(i = -76; i < 76; i++)
+   {
+   ctx.lineTo(i , -i/4 * i/4 * i/4);
+   }
+
+   ctx.restore();
+   ctx.lineJoin = "round";
+   ctx.strokeStyle = "red";
+   ctx.lineWidth = 2;
+   ctx.stroke();
+   ctx.restore();
+   ctx.translate(-300, -300)
+ }
+
+}
